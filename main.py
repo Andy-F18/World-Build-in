f0c59389d3a4bb1c59cf40e2d6de44e389b2f52a
Andy F18 <adrian.f18e@gmail.com>
2022-07-28 22:40:19 +0200

    first draw 2

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import os
 class WolrdBuildIn:
     def __init__(self):
         # #################### BEGIN INIT VARS ####################
-        self.size = {'h': 700, 'w': 1000}
-        self.colors = {'bg_a': '#ccc', 'bg_na': '#', 'menu1': '#555555'}
+        self.size = {'h': 700, 'w': 1300}
+        self.colors = {'bg1': '#ccc', 'menu1': '#555555', 'menu2': '#666666'}
         # #################### END INIT VARS ####################
 
         try:
@@ -15,22 +15,24 @@ class WolrdBuildIn:
             pass
 
         # #################### BEGIN WINDOW ####################
-        self.root = tk.Tk()
-        self.root.title("World Build'in")
+        self.__root = tk.Tk()
+        self.__root.title("World Build'in")
 
-        self.root.configure(background=self.colors['bg_a'])
-        p_r = int(self.root.winfo_screenwidth() / 2 - self.size['w'] / 2)
-        p_d = int(self.root.winfo_screenheight() / 2 - self.size['h'] / 2-50)
-        self.root.geometry("{}x{}+{}+{}".format(self.size['w'], self.size['h'], p_r, p_d))
+        self.__root.configure(background=self.colors['bg1'])
+        p_r = int(self.__root.winfo_screenwidth() / 2 - self.size['w'] / 2)
+        p_d = int(self.__root.winfo_screenheight() / 2 - self.size['h'] / 2 - 50)
+        self.__root.geometry("{}x{}+{}+{}".format(self.size['w'], self.size['h'], p_r, p_d))
         # #################### END WINDOW ####################
 
         self.__menu_p()
         self.__menu_h()
+        self.__currentFrame = tk.Frame(self.__root)
+        self.__currentFrame.pack()
 
-        self.root.mainloop()
+        self.__root.mainloop()
 
     def __menu_p(self):
-        menu = tk.Menu(self.root)
+        menu = tk.Menu(self.__root)
 
         fileMenu = tk.Menu(menu, tearoff=0, activebackground=self.colors['menu1'])
         fileMenu.add_command(label='New world')
@@ -38,26 +40,77 @@ class WolrdBuildIn:
         fileMenu.add_command(label='Save')
         fileMenu.add_command(label='Save as')
         fileMenu.add_separator()
-        fileMenu.add_command(label='Exit', command=self.root.quit)
+        fileMenu.add_command(label='Exit', command=self.__root.quit)
         menu.add_cascade(label='File', menu=fileMenu)
 
         menu.add_command(label='Add')
 
-        self.root.config(menu=menu)
+        self.__root.config(menu=menu)
 
     def __menu_h(self):
-        f_menue = tk.Frame(self.root, background=self.colors['menu1'])
+        f_menue = tk.Frame(self.__root, background=self.colors['menu1'])
+        w = int(self.size['w']/7.1)//4
 
-        b_cara = tk.Button(f_menue, text="Personnage", relief='flat', width=25)
-        b_obj = tk.Button(f_menue, text="Objets", relief='flat', width=25)
-        b_place = tk.Button(f_menue, text="Lieux", relief='flat', width=25)
+        b_chara = tk.Button(f_menue, text="Character", relief='flat', width=w, command=self.__charPage,
+                            background=self.colors['menu1'], activebackground=self.colors['menu2'])
+        b_place = tk.Button(f_menue, text="Locations", relief='flat', width=w, command=self.__placePage,
+                            background=self.colors['menu1'], activebackground=self.colors['menu2'])
+        b_items = tk.Button(f_menue, text="Items", relief='flat', width=w, command=self.__itemPage,
+                            background=self.colors['menu1'], activebackground=self.colors['menu2'])
+        b_story = tk.Button(f_menue, text="Story", relief='flat', width=w,command=self.__storyPage,
+                            background=self.colors['menu1'], activebackground=self.colors['menu2'])
 
         self.__import_file = tk.StringVar()
-        b_cara.grid(column=0, row=0)
-        b_obj.grid(column=1, row=0)
-        b_place.grid(column=2, row=0)
+        b_chara.grid(column=0, row=0)
+        b_place.grid(column=1, row=0)
+        b_items.grid(column=2, row=0)
+        b_story.grid(column=3, row=0)
 
         f_menue.pack(anchor="w")
+
+    def __charPage(self):
+        self.__currentFrame.destroy()
+        charFrame = tk.Frame(self.__root, background=self.colors['bg1'])
+
+        listFrame = tk.Frame(charFrame, background=self.colors['bg1'])
+        tk.Label(listFrame, text='Characters', bg=self.colors['bg1']).pack()
+        listFrame.pack(anchor=tk.NW)
+
+        charFrame.pack()
+        self.__currentFrame = charFrame
+
+    def __placePage(self):
+        self.__currentFrame.destroy()
+        placeFrame = tk.Frame(self.__root, background=self.colors['bg1'])
+
+        listFrame = tk.Frame(placeFrame, background=self.colors['bg1'])
+        tk.Label(listFrame, text='Places', bg=self.colors['bg1']).pack()
+        listFrame.pack(anchor=tk.NW)
+
+        placeFrame.pack()
+        self.__currentFrame = placeFrame
+
+    def __itemPage(self):
+        self.__currentFrame.destroy()
+        itemFrame = tk.Frame(self.__root, background=self.colors['bg1'])
+
+        listFrame = tk.Frame(itemFrame, background=self.colors['bg1'])
+        tk.Label(listFrame, text='Item', bg=self.colors['bg1']).pack()
+        listFrame.pack(anchor=tk.NW)
+
+        itemFrame.pack()
+        self.__currentFrame = itemFrame
+
+    def __storyPage(self):
+        self.__currentFrame.destroy()
+        storyFrame = tk.Frame(self.__root, background=self.colors['bg1'])
+
+        listFrame = tk.Frame(storyFrame, background=self.colors['bg1'])
+        tk.Label(listFrame, text='Story', bg=self.colors['bg1']).pack()
+        listFrame.pack(anchor=tk.NW)
+
+        storyFrame.pack()
+        self.__currentFrame = storyFrame
 
 
 if __name__ == '__main__':
